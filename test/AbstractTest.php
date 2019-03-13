b8238b90c0a298e38c605ae8a554f98cfd7493be
romeritoCL <romero.latorre@gmail.com>
2019-03-13 14:57:41 +0100

    Updated wait to 3 minutes

--- test/AbstractTest.php
+++ test/AbstractTest.php
@@ -33,7 +33,7 @@ abstract class AbstractTest extends TestCase
     protected function setUp()
     {
         $this->webDriver = RemoteWebDriver::create(
-            'http://selenium:4444/wd/hub',
+            'http://localhost:4444/wd/hub',
             DesiredCapabilities::chrome(),
             180000,
             180000
