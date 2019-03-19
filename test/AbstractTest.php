6501367b6261428fb91d6a4002b1d7c77c0bb8d0
your github username <yourmail@digitalorigin.com>
2019-03-19 11:26:29 +0100

    increase timeout

--- test/AbstractTest.php
+++ test/AbstractTest.php
@@ -98,7 +98,7 @@ abstract class AbstractTest extends TestCase
      */
     public function waitUntil(WebDriverExpectedCondition $condition)
     {
-        return $this->webDriver->wait(5, 200)->until($condition);
+        return $this->webDriver->wait(50, 250)->until($condition);
     }
 
     /**
