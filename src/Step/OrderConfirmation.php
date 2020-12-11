1ce31df80630c6e6cf9b817638308b96a1fcf7c0
pgarces <37547687+pgarcess@users.noreply.github.com>
2020-12-11 10:57:54 +0100

    RiskContainer on form

--- src/Step/OrderConfirmation.php
+++ src/Step/OrderConfirmation.php
@@ -42,20 +42,28 @@ class OrderConfirmation extends AbstractStep
             return false;
         }
 
-        //Click on popup
-        sleep(3);
-        $buttonElementSearch = WebDriverBy::name('dialogButton');
-        $condition  = WebDriverExpectedCondition::presenceOfElementLocated($buttonElementSearch);
-        $this->webDriver->wait()->until($condition);
-        $formContinue = $this->webDriver->findElement($buttonElementSearch)->click();
-        sleep(3);
+        try{
+            //Click on popup
+            sleep(3);
+            $buttonElementSearch = WebDriverBy::name('dialogButton');
+            $condition  = WebDriverExpectedCondition::presenceOfElementLocated($buttonElementSearch);
+            $this->webDriver->wait()->until($condition);
+            $formContinue = $this->webDriver->findElement($buttonElementSearch)->click();
+            sleep(3);
 
-        //Click on confirm
-        $buttonElementSearch = WebDriverBy::className('button-contained-primary');
-        $condition  = WebDriverExpectedCondition::presenceOfElementLocated($buttonElementSearch);
-        $this->webDriver->wait()->until($condition);
-        $formContinue = $this->webDriver->findElement($buttonElementSearch)->click();
-        sleep(3);
+            //Click on confirm
+            $buttonElementSearch = WebDriverBy::className('button-contained-primary');
+            $condition  = WebDriverExpectedCondition::presenceOfElementLocated($buttonElementSearch);
+            $this->webDriver->wait()->until($condition);
+            $formContinue = $this->webDriver->findElement($buttonElementSearch)->click();
+            sleep(3);
+        } catch (\Exception $e) {
+            $errorElementSearch = WebDriverBy::className('riskPageContainer');
+            $condition  = WebDriverExpectedCondition::presenceOfElementLocated($errorElementSearch);
+            $this->webDriver->wait()->until($condition);
+            $this->assertTrue((bool) $condition, "ERROR ON ".self::STEP);
+            return false;
+        }
 
         return false;
     }
